26c70f6aaa4747d5ec07d3124a177d8ab796fa73
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-11 14:22:55 -0400

    lab done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,8 @@
-def parrot():
+# import pytest
+#  use "pipenv shell" for testing
+# use pdb a debugger 
+
+def parrot(my_string="Squawk!"):  # optional parameter, you can call it with the param as parrot('hello') or one without it 
     pass
+    print(my_string)
+    return my_string
